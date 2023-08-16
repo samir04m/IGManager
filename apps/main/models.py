a0de1abdd12a876ba12e3816a8b0b3893f9f4719
Samir Mejia <samir04m.dev@gmail.com>
2023-08-16 18:52:35 -0500

    Creacion modelos y migracion

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    order = models.IntegerField(default=0)
+    def __str__(self):
+        return "{}".format(self.name)
+
+class Username(models.Model):
+    username = models.CharField(max_length=50)
+    info = models.TextField(max_length=300, null=True, blank=True)
+    state = models.ForeignKey(State, null=False, blank=False, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['username']
+    def __str__(self):
+        return "{}".format(self.username)
