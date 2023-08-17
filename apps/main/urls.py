3ad79bec11be0d20fb93e5b2f51cff44036c7567
Samir Mejia <samir04m.dev@gmail.com>
2023-08-17 01:17:33 -0500

    Update and delete username

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,5 +5,8 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('create-username/', create_username, name='create_username'),
+    path('u/<str:username>/', view_username, name='view_username'),
+    path('u/<str:username>/del/', delete_username, name='delete_username'),
+    path('update-username/<int:username_id>/', update_username, name='update_username'),
     path('change-state/<int:username_id>/', change_state, name='change_state'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -14,24 +14,27 @@ def create_username(request):
         igUsername = getUsernameFromUrl(request.POST.get('url'))
         username = Username.objects.filter(username=igUsername).first()
         if username:
-            messages.info(request, 'Username already exists', extra_tags='info')
+            messages.info(request, 'Username is currently in "{}"'.format(username.state.name), extra_tags='info')
         else:
-            try:
-                username = Username(
-                    username = igUsername,
-                    state = State.objects.get(order=1)
-                )
-                username.save()
-                messages.success(request, 'Username saved', extra_tags='success')
-            except Exception as ex:
-                messages.error(request, 'Failed to add username', extra_tags='error')
+            if igUsername:
+                try:
+                    username = Username(
+                        username = igUsername,
+                        state = State.objects.get(order=1)
+                    )
+                    username.save()
+                    messages.success(request, 'Username saved', extra_tags='success')
+                except Exception as ex:
+                    messages.error(request, 'Failed to add username', extra_tags='error')
     return redirect(request.META.get('HTTP_REFERER'))
 
 def getUsernameFromUrl(url:str) -> str:
     parsed_url = urlparse(url)
     path_parts = parsed_url.path.split("/")
-    username = path_parts[-1]
-    return username
+    if len(path_parts) == 1:
+        return path_parts[0]
+    else:
+        return path_parts[1]
 
 def change_state(request, username_id):
     username = get_object_or_404(Username, id=username_id)
@@ -39,4 +42,32 @@ def change_state(request, username_id):
     username.state = get_object_or_404(State, order=nState+1)
     username.save()
     return redirect(request.META.get('HTTP_REFERER'))
+
+def view_username(request, username):
+    username = get_object_or_404(Username, username=username)
+    states = State.objects.all()
+    return render(request, 'main/view_username.html', {'username':username, 'states':states})
+
+def update_username(request, username_id):
+    if request.method == 'POST':
+        username = get_object_or_404(Username, id=username_id)
+        updated = False
+        if int(request.POST.get('state')) != username.state.id:
+            username.state = State.objects.get(id=int(request.POST.get('state')))
+            updated = True
+        if request.POST.get('info') != username.info:
+            print("entro ------")
+            username.info = request.POST.get('info')
+            updated = True
+        if updated:
+            username.save()
+            messages.success(request, 'Username updated', extra_tags='success')
+    return redirect(request.META.get('HTTP_REFERER'))
+
+def delete_username(request, username):
+    username = get_object_or_404(Username, username=username)
+    username.delete()
+    uname = username.username
+    messages.success(request, 'Username "{}" was removed'.format(uname), extra_tags='success')
+    return redirect('main:home')
 
